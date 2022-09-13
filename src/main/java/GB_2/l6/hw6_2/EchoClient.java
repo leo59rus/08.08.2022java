592223046020bfcda770bfbc91768e9afb47ca2c
leonid <orangr59usa@gmail.com>
2022-09-13 12:24:44 +0300

    чат сервер клиент начало

---
+++ src/main/java/GB_2/l6/hw6_2/EchoClient.java
@@ -0,0 +1,93 @@
+package GB_2.l6.hw6_2;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class EchoClient {
+    private Socket socket;
+    private DataInputStream in; //для приема сообщений
+    private DataOutputStream out;//для отправки сообщений
+
+
+    public static void main(String[] args) {
+        new EchoClient().start();
+    }
+
+    private void start() {
+        try {
+            openConnection();//метод открытия соединения с сервером
+            Scanner scanner = new Scanner(System.in);
+            while (!socket.isClosed()) {
+                sendMessage(scanner.nextLine());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void sendMessage(String message) {
+        try{
+            out.writeUTF(message);
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    //открытие соединение с сервером
+    private void openConnection() throws IOException {
+        //ip adress адрес локальной машины(host) моей, везде такой
+        // один
+        //порт как у сервера
+        socket = new Socket("127.0.0.1", 8189);
+        //создаем вход выход сообщений
+        in = new DataInputStream(socket.getInputStream());
+        out = new DataOutputStream(socket.getOutputStream());
+        new Thread(() -> {
+            try {
+                while (true) {
+                    final String message = in.readUTF();//ждем
+                    // сообщение от
+                    // сервера
+                    if ("/end".equalsIgnoreCase(message)) {
+                        break;
+                    }
+                    System.out.println("Сообщение от сервера: " + message);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }finally {
+                closeConnection();
+
+                }
+
+        }).start();
+    }
+
+    private void closeConnection() {
+        if(in != null){
+            try {
+                in.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        if (out!= null){
+            try {
+                out.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        if (socket!=null){
+            try {
+                socket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+}
---
+++ src/main/java/GB_2/l6/hw6_2/EchoServer.java
@@ -0,0 +1,48 @@
+package GB_2.l6.hw6_2;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class EchoServer {
+    public static void main(String[] args) {
+        //IPv4 192.168.0.1 // 32 бита 4 байта
+        //IPv6             // 128бит 16 байтов
+
+        //оборачиваем сервер в искл try-with-resourses на закрытие в
+        // случае
+        // неиспользования порта
+        try (ServerSocket serverSocket = new ServerSocket(8189)) {
+            //cлушает на порте 8189 //от0 до 65000 но
+            // больше 1024 тк они заняты
+            System.out.println("ждем подключения клиента...");
+            final Socket socket = serverSocket.accept();
+            System.out.println("клиент подключился");
+
+            //для чтения
+            final DataInputStream in =
+                    new DataInputStream(socket.getInputStream());
+            //для отправки
+            final DataOutputStream out =
+                    new DataOutputStream(socket.getOutputStream());
+
+            while(true){//poison pill
+                final String message = in.readUTF();
+                //при отправке енд отправляется клиенту
+                // заканчивается цикл отправки сообщений
+                if("/end".equalsIgnoreCase(message)){
+                    out.writeUTF("/end");
+                    break;
+                }
+                System.out.println("сообщение от клиента: " + message);
+                out.writeUTF("[echo] " + message);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
